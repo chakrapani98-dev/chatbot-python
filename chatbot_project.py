c34e4df4e17f8501734e03c4b0b89123e59e912d
chakrapani98-dev <chakrapani705@gmail.com>
2025-09-30 23:36:44 -0700

    full chatbot base with streaming, state graph with conversation limit

--- chatbot_project.py
+++ chatbot_project.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
-from langchain_core.messages import HumanMessage,SystemMessage
+from langchain_core.messages import HumanMessage,SystemMessage,AIMessage
+from langchain_core.messages import SystemMessage, trim_messages
 
 
 load_dotenv()
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 # --- langchain setup----
 groq_api_key=os.getenv("GROQ_API_KEY")
 
@@ -20,10 +22,21 @@ model = init_chat_model("llama-3.3-70b-versatile", model_provider="groq")
 # Define a new graph
 workflow = StateGraph(state_schema=MessagesState)
 
+trimmer = trim_messages(
+    max_tokens=65,
+    strategy="last",
+    token_counter=model,
+    include_system=True,
+    allow_partial=False,
+    start_on="human",
+)
+
 # Define the function that calls the model
 def call_model(state: MessagesState):
-    response = model.invoke(state["messages"])
+    trimmed_messages = trimmer.invoke(state["messages"])
+    response = model.invoke(trimmed_messages)
     return {"messages": response}
+
 
 # Define the (single) node in the graph
 workflow.add_edge(START, "model")
@@ -36,11 +49,20 @@ config = {"configurable": {"thread_id": "abc123"}}
 
 #model main invoke code
 input_msg="whats your name"
+language="English"
 while input_msg!="exit":
-  input_msg=input("Ask:",)
-  messages = [
+    input_msg=input("Ask:",)
+    messages = [
     # SystemMessage(content="You are a helpful assistant! Your name is Bob."),
     HumanMessage(content=input_msg),]
-  output_msg=app.invoke({"messages": messages}, config)
-  output_msg["messages"][-1].pretty_print()
-
+    for chunk, metadata in app.stream(
+    {"messages": messages, "language": language},
+    config,
+    stream_mode="messages",
+):
+     print(chunk.content, end="", flush=True)
+    print()
+    #  if isinstance(chunk, AIMessage):  # Filter to just model responses
+    #       print(chunk.content, end="|")
+#   output_msg=app.invoke({"messages": messages}, config)
+#   output_msg["messages"][-1].pretty_print()
